4424029f90a427c1e3ba8358df6a8a0a9e07ca34
anstonebear <anstonebear@mail.ru>
2024-12-12 17:30:42 +0300

    layout header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+
+import Header from '../widgets/header/header';
+
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Header />
+		</div>
+	);
 }
 
 export default App;
