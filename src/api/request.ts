57f9735e0ca068d0da407842d97e4e240dc8d0b5
anstonebear <anstonebear@mail.ru>
2025-01-28 21:32:01 +0300

    axios

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,8 +1,20 @@
-import axios from 'axios'
+import axios, { isAxiosError } from 'axios'
 
 const API_URL = 'https://platform-courses.emgushovs.ru/api'
 
-export const dataCourses = axios({
-	url: `${API_URL}/course`,
-	method: 'GET'
-})
+export const dataCourses = async () => {
+	try {
+		const response = await axios.get(`${API_URL}/course`)
+		return response.data
+	} catch (error) {
+		if (isAxiosError(error)) {
+			console.log(error.response?.data, 'RI_Error fetching courses')
+		} else if (error instanceof Error) {
+			console.log(error.message, 'RI_Error ')
+		}
+	}
+}
+// axios({
+// 	url: `${API_URL}/course`,
+// 	method: 'GET'
+// })
--- src/widgets/coursesCards/coursesCards.tsx
+++ src/widgets/coursesCards/coursesCards.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import { useEffect, useState } from 'react'
+// import axios from 'axios'
 import { Container } from '~/shared/ui/container'
 import CoursesCard from './coursesCard'
 import { image1, image2, image3 } from '~/shared/public/index'
@@ -7,34 +7,61 @@ import { dataCourses } from '~/api/request'
 
 import style from './coursesCards.module.scss'
 
-const CoursesCards = () => {
-	console.log(dataCourses, 'dataCourses')
+interface ICoursesProps {
+	id: number
+	title: string
+	description: string
+}
+
+const CoursesCards: React.FC = () => {
+	const [courses, setCourses] = useState<ICoursesProps[]>([])
+
+	useEffect(() => {
+		dataCourses().then(response => {
+			setCourses(response)
+		})
+	}, [])
+
+	// useEffect(() => {
+	// 	fetch('https://platform-courses.emgushovs.ru/api/course')
+	// 		.then(res => {
+	// 			return res.json()
+	// 		})
+	// 		.then(arr => {
+	// 			setCourses(arr)
+	// 		})
+	// }, [])
+
+	// useEffect(() => {
+	// 	axios.get('https://platform-courses.emgushovs.ru/api/course')
+	// 	  .then(response => {
+	// 		setCourses(response.data);
+	// 	  })
+	//   }, [])
+
+	// console.log(dataCourses(), 'RI_dataCourses')
 	return (
 		<div className={style.coursesCards}>
 			<Container>
 				<div className={style.coursesCards_wrapper}>
-					{Array(6)
-						.fill(null)
-						.map((course, id) => (
-							<CoursesCard
-								key={id}
-								img1={image1}
-								img2={image2}
-								img3={image3}
-								title={`Фундаментальный веб-дизаин`}
-								descr={`Изучите основы веб-дизайна, включая HTML, CSS и принципы адаптивного
-					дизайна. Развивайте навыки создания визуально привлекательных и
-					удобных для пользователя веб-сайтов`}
-								experience={`начинающий`}
-								level={`4 недели`}
-								teacher={`by Jhon Smith`}
-								lesson1={`Введение в HTML`}
-								lesson2={`CSS стилизация`}
-								lesson3={`Введение в адаптивный дизаин`}
-								lesson4={`Принципы веб-дизайна`}
-								lesson5={`Создание веб-сайта`}
-							/>
-						))}
+					{courses.map(course => (
+						<CoursesCard
+							key={course.id}
+							img1={image1}
+							img2={image2}
+							img3={image3}
+							title={course.title}
+							descr={course.description}
+							experience={`начинающий`}
+							level={`4 недели`}
+							teacher={`by Jhon Smith`}
+							lesson1={`Введение в HTML`}
+							lesson2={`CSS стилизация`}
+							lesson3={`Введение в адаптивный дизаин`}
+							lesson4={`Принципы веб-дизайна`}
+							lesson5={`Создание веб-сайта`}
+						/>
+					))}
 				</div>
 			</Container>
 		</div>
